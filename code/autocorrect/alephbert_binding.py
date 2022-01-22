0449d44c2a9fccddc38a74251bc1552a8a4d4392
saifun <saifunny@gmail.com>
2022-01-22 19:25:06 +0200

    Autocorrect core code works

--- code/autocorrect/alephbert_binding.py
+++ code/autocorrect/alephbert_binding.py
@@ -38,6 +38,13 @@ class AlephBertPredictor:
         suggested_tokens = alephbert.get_suggestions_by_probability(text, token_index_to_correct)
         return sorted(suggested_tokens, key=lambda token: token.get_suggested_token_score(masked_token), reverse=True)
 
+    def get_whole_sentence_autocorrect(self, text):
+        suggested_sentence = []
+        for index, word in enumerate(text.split()):
+            autocorrect_suggestions = alephbert.get_autocorrect_suggestions(text, index)
+            suggested_sentence.append(autocorrect_suggestions[0].value)
+        return ' '.join(suggested_sentence)
+
     def _get_masked_token_suggestions(self, model_output_tensors, token_index_to_mask, amount_of_suggestions):
         masked_token_tensor = model_output_tensors[0][token_index_to_mask + 1]
         probabilities = softmax(masked_token_tensor, dim=0)
@@ -55,10 +62,6 @@ class AlephBertPredictor:
         split_text = text.split()
         split_text[index] = self.alephbert_tokenizer.mask_token
         return ' '.join(split_text)
-
-    def _get_token_correctness_score(self, masked_token, suggested_tokens):
-        if any([token.calculate_fuzziness_for_token(masked_token) for token in suggested_tokens]):
-            return 1
 
 
 if __name__ == '__main__':
